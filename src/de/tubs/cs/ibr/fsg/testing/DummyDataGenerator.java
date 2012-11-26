0293f67f36088ed9b1a08357362df0f8bb420df5
Eduardo2012 <eduardo_sisco@yahoo.de>
2012-11-26 18:56:46 +0100

    Anpassungen der GUI und der JSON-Formate für die Blacklists

---
+++ src/de/tubs/cs/ibr/fsg/testing/DummyDataGenerator.java
@@ -0,0 +1,56 @@
+package de.tubs.cs.ibr.fsg.testing;
+
+
+
+/**
+ * Diese Klasse wird nur zum Testen gebraucht, am Ende des Projektes kann sie gelöscht werden.
+ * 
+ */
+public class DummyDataGenerator {
+
+	public static final int DRIVERS_DATA_TYPE = 1;
+	public static final int TEAMS_DATA_TYPE =   2;
+	
+	private static final String DRIVER_XML = "{\"UserID\":18664,\"TeamID\":33," +
+			"\"first_name\":\"Alexander\",\"last_name\":\"Mustermann\",\"gender\":0}";
+	
+	private static final String TEAM_XML =   "{\"TeamID\":33,\"CN\":\"AT\"," +
+			"\"cn_short_en\":\"Austria\",\"city\":\"Graz\",\"U\":\"TU\",\"Car\":59," +
+			"\"Pit\":49,\"iswaiting\":0,\"class\":1,\"name_pits\":\"TU Graz\"}";
+	
+
+	/**
+	 * Mit dieser Methode werden Dummy-Daten generiert, die beim Testen der IBR-DTN-Funktionalität
+	 * gebraucht werden. Es geht um die Größe der Daten, die transportiert werden sollen.
+	 * 
+	 * @param amount Anzahl der Dummy-Daten, die generiert werden sollen.
+	 * @param dataTyp Hier kann man zurzeit zwischen Fahrer und Teams auswählen ("DRIVERS_DATA_TYPE" und "TEAMS_DATA_TYPE").
+	 * @return Die fertig generierte Dummy-Daten in JSON-Format.
+	 * @throws Exception Ein Fahler sollte nur dann auftreten, wenn Benutzer der Methode nicht die static Konstanten "DRIVERS_DATA_TYPE" und "TEAMS_DATA_TYPE" benutzen sondern selbst einen Integer-Wert angeben.
+	 */
+	public static String createDataSet(int amount, int dataTyp) throws Exception{
+		String result = "[";
+		String data_xml = "";
+		
+		if (dataTyp==DummyDataGenerator.DRIVERS_DATA_TYPE){
+			data_xml= DRIVER_XML;
+		}else if(dataTyp==DummyDataGenerator.TEAMS_DATA_TYPE){
+			data_xml= TEAM_XML;
+		}else{
+			throw new Exception("Weder Fahrer noch Teams ausgewählt, bitte ihre dataTyp-Angabe überprüfen!");
+		}
+		
+		for(int i=0; i<amount; i++){
+			result = result + data_xml;
+			if (i<amount-1){
+				result = result + ",";
+			}
+		}
+		
+		result = result + "]";
+		return result;
+	}
+	
+
+	
+}
